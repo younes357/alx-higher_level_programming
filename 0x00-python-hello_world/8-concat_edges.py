4c405154a9aefe9c811d28b97e349cc044236c85
Younes <bdryounesgh@gmail.com>
2024-04-27 08:10:06 -0700

    concat edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object-oriented programming") : str.find("language")] + str[str.find("with") : str.find("very")] + str[: str.find("is")]
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
