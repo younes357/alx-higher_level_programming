e6fe27a29e6138d202dd2f57b578be3e427dec51
Younes <bdryounesgh@gmail.com>
2024-05-07 15:27:25 -0700

    pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
     a1 = a
-    for i in range(b - 1):
-        a = a*a1
+    if a > 0 and b > 0:
+        for i in range(b - 1):
+            a = a*a1
+    elif a > 0 and b < 0:
+        for i in range(-b - 1):
+            a = a*a1
+        a = 1 / a
     return a
