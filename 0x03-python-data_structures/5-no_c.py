78308718f7f318dc3ec92eec71abc3eb0f603408
Younes <bdryounesgh@gmail.com>
2024-05-19 13:49:52 -0700

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    for i in range(len(my_string - 1):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            del my_string[i]
+return my_string
+
