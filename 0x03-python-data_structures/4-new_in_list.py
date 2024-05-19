2c7a44308c6aa5745041189a6fb25948429a0700
Younes <bdryounesgh@gmail.com>
2024-05-19 13:08:58 -0700

    replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        lis = my_list
+        lis = list(my_list)
         lis[idx] = element
         return lis
