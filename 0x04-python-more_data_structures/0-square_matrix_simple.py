e805f3c41238b50ab48d0d5d048831ca7bc0fd8c
Younes <bdryounesgh@gmail.com>
2024-06-10 16:34:04 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     res = []
-    res1 = []
-    res2 = []
-    res = list(map(lambda x: x*x, matrix[0]))
-    res1 = list(map(lambda x: x*x, matrix[1]))
-    res2 = list(map(lambda x: x*x, matrix[2]))
-    return [res, res1, res2]
+    for i in matrix :
+        subres = list(map(lambda x: x*x, i))
+        res.append(subres)
+    return res
